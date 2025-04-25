ae56f637cc8d365977543ab8401cda05663fe03e
Miguel Romero Carmona <miguel.romero.carmona@gmail.com>
2025-04-25 14:22:36 +0200

    feat: mejora viernes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,14 @@
 // lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
-export default prisma;
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+    })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
